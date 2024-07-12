74c9ae0b16c7a7280901bfe94f11e729427ec05d
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-07-13 00:57:25 +0200

    Krijimi i pjeses se Projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import styles from './ProjectsStyles.module.css'
 import grieta from '../../assets/logo-color.png'
+import ffk from '../../assets/Logo.png'
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-          <a href="https://github.com/shubi06/GrietaHospital-WebProject" target='_blank'>
-          <img className="hover" src={grieta} alt="" />
-          <h3>Grieta</h3>
-          <p>Medical Website</p>
-          </a>
+        <ProjectCard src={grieta}   
+        link={'https://github.com/shubi06/GrietaHospital-WebProject'}
+        h3="Grieta Hospital"
+        p="Website for a Hospital"
+        />
+        
+        <ProjectCard src={ffk} 
+        link={'https://github.com/Gentrit2023/Lab1-Final-'}
+        h3="Ticket System"
+        p="Web Application for Football Federation of Kosovo"
+
+        />
         </div>
     </section>
   );
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import styles from './SkillsStyles.modules.css'
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1>Skills</h1>
+    </section>
+  )
+}
+
+export default Skills
